65003a78130de8f52df7f79fc6453d6862144958
Yogi Sulistianto <sutodiryo@users.noreply.github.com>
2021-07-26 16:52:27 +0700

    games

---
+++ routes.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$route['default_controller']    = 'guest';
+$route['karir']                 = 'karir/index';
+$route['2']                     = 'guest/homepage2';
+$route['admin']                 = 'admin/dashboard';
+$route['404_override']          = '';
+$route['translate_uri_dashes']  = FALSE;
